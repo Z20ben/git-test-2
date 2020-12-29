941b81294926ba742db294a09ae204cab1f346d2
Fredika Benz <benedictbenz.kerja@gmail.com>
2020-12-29 18:30:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci dolores laboriosam iure. Neque optio recusandae quae. Quaerat doloribus in veniam tempora magni, ex ut, ipsa molestiae nemo quod dicta non.</p>
+    <ul>
+        <li>1</li>
+    </ul>
 </body>
 
 </html>
